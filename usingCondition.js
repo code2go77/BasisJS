6f38a05e743989de4049dad3988a0d131585a830
code2go77 <code2go77@gmail.com>
2024-05-19 12:42:37 +0400

    conditional loop in js

--- usingCondition.js
+++ usingCondition.js
@@ -14,3 +14,39 @@ else if (hour >= 12 && hour < 18) {
 else {
     console.log('Good Evening')
 }
+
+// Using For loop in JS : To find the Odd No's
+
+for (let i = 1; i <=5; i++) {
+    if (i % 2 !== 0) console.log('Odd No: ', i)
+}   
+
+for (let i = 5; i >=1; i--) {
+    if (i % 2 !== 0) 
+        console.log('Odd No: ', i)
+} 
+
+// Using For loop in JS : To find the Odd No's
+for (let i = 1; i <=5; i++) {
+    if (i % 2 === 0)
+        console.log('Even No: ', i)
+}
+
+//Using For-in
+const person = { // person Object with its properties.
+    name : 'Vikas',
+    age : 30
+}
+
+for (let key in person)
+    console.log(key, person[key]);
+
+//Using Array For-In example.
+
+const color = ['red', 'blue', 'green'];
+for (let index in color)
+    console.log(color[index])
+let x=0;
+//using For-of loop latest
+for (let colors of color)
+    console.log(colors)
